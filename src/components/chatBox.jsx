eca2795257bd62b357a93812d94c8ada7c560eee
Sibsankar-de <sibsankarde910@gmail.com>
2024-06-20 00:07:24 +0530

    commit 8

--- src/components/chatBox.jsx
+++ src/components/chatBox.jsx
@@ -464,7 +464,7 @@ const ChatBubble = ({ messageFrom, messageItem, messageList, index, currentUser 
 
   const handleFileDownload = async () => {
     try {
-      const downloadUrl = message?.content?.file?.url
+      const downloadUrl = getImgDownloadUrl(message?.content?.file?.url)
       const response = await fetch(downloadUrl, { mode: 'cors' });
       const blob = await response.blob();
       const url = window.URL.createObjectURL(blob);
@@ -477,7 +477,7 @@ const ChatBubble = ({ messageFrom, messageItem, messageList, index, currentUser 
       document.body.removeChild(link);
       window.URL.revokeObjectURL(url);
     } catch (error) {
-      // console.error('Download failed:', error);
+      console.error('Download failed:', error);
     }
   };
 
